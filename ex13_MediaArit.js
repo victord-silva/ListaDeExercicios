c3f7e92176e9b1d8d0a1a5318aa2a1c1ad8dbd8d
victord-silva <victord.silva0206@gmail.com>
2024-08-30 22:18:22 -0300

    Ex13 Feito

--- ex13_MediaArit.js
+++ ex13_MediaArit.js
@@ -3,18 +3,25 @@ const prompt = require("prompt-sync")(); // Inicializa o prompt
 // 13. Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer
 // a média aritmética desses números.  
 
-function arithmeticMean() {
-    let actualNum
-    let count
+console.log("Faça a inserção de números decimais para fazer a média aritmética, quando não quiser mais aperte [0]")
 
-    while(num != 0) {
-        let num = Number(prompt("Insira um número decimal: "))
-        actualNum = actualNum + num
-        count++
+function avg() { // Função para poder retornar as variáveis fora do "While"
+
+    let sumNum = 0 // No while inica-se um valor para a variavel antes para que não retorne undefined
+    let count = 0 // No while inica-se um valor para a variavel antes para que não retorne undefined
+    let num = 1 // Define-se como 1 para poder entrar sempre no while
+
+    while(num !== 0) { // Repete o prompt até que o usuário digite 0
+        num = Number(prompt("Insira um número decimal: ")) // Recebe o valor do usuário
+        sumNum = sumNum + num // Soma o valor fornecido pelo usuário em outra variável
+        count++ // Incrementa a cada loop para poder calcular a media aritmética
+        if (sumNum === 0) { // Caso o usúario passe 0 na primeira requisição, ja irá cair nessa condicional, pois sumNum será 0 no primeiro loop
+            return console.log("A média aritmética é igual a 0!")
+        }
     }
-
-    let calc = actualNum / count
-    console.log(calc)
+    count-- // Tirar um incremento, pois o loop irá rodar mais uma vez após o usuário apertar 0 e caso o usuário digite 1 número só, a lógica iria dividir por 2
+    let result = sumNum / count 
+    return console.log(`A média aritmética é igual a ${result.toFixed(2)}`) // Não consegui tratar para que só recebe números decimais, mas transformei o resultado em decimais.
 }
 
-arithmeticMean()
+avg() // Chama a função para executar a lógica
